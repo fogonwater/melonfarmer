c2519c661a9b285b7cf1d56201cc2a0b66cb9d9c
Chris McDowall <chris.mcdowall@gmail.com>
2017-06-10 13:27:46 +1200

    Move functions out of Class.

--- melon.py
+++ melon.py
@@ -1,12 +1,72 @@
 # -*- coding: utf-8 -*-
 
 import csv, json, os, time
-import requests
+try:
+    import requests
+except ImportError:
+    pass
 
 def set_default(obj):
     if isinstance(obj, set):
         return list(obj)
     raise TypeError
+
+def touch(f_path='index.html', times=None):
+    with open(f_path, 'a'):
+        os.utime(f_path, times)
+
+def get_filenames(f_dir, prefix='', suffix=''):
+    f_names = []
+    for r,d,files in os.walk(f_dir):
+        for f in files:
+            if f.startswith(suffix) and f.endswith(suffix):
+                f_names.append('{}/{}'.format(r, f))
+    return f_names
+
+def file_exists(f_path):
+    if os.path.isfile(f_path):
+        return True
+    return False
+
+def get_json(url, verbose=True):
+    if verbose:
+        print('Fetching: {}'.format(url))
+    r = requests.get(url)
+    if r.status_code != 200:
+        print('Error: {}.'.format(r.status_code))
+    return r.json()
+
+def read_csv(f_path, fieldnames=[]):
+    with open(f_path) as f:
+        if not fieldnames:
+            reader = csv.DictReader(f)
+        else:
+            reader = csv.DictReader(f, fieldnames=fieldnames)
+        rows = [ row for row in reader ]
+    return rows
+
+def read_json(f_path):
+    with open(f_path, 'r') as infile:
+        data = json.load(infile)
+    print('Read {}.'.format(f_path))
+    return data
+
+def write_json(data, f_path):
+    with open(f_path, 'w') as outfile:
+        json.dump(
+            data,
+            outfile,
+            indent=2,
+            sort_keys=True
+        )
+    print('{} written.'.format(f_path))
+
+def list_to_csv(data, f_path):
+    with open(f_path, 'wb') as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+    print('Wrote {}.'.format(f_path))
+
 
 
 class Farmer:
@@ -80,16 +140,16 @@ class Farmer:
             print('Wrote {}.'.format(f_path))
 
     def get_filenames(self, f_dir, suffix=''):
-        f_names = []
+        f_paths = []
         for r,d,files in os.walk(f_dir):
             for f in files:
                 if f.endswith(suffix):
-                    f_names.append('%s/%s' % (r, f))
-        return f_names
+                    f_paths.append('%s/%s' % (r, f))
+        return f_paths
 
-    def file_exists(self, fname):
-        if os.path.isfile(fname):
+    def file_exists(self, f_path):
+        if os.path.isfile(f_path):
             if self.verbose:
-                print(' * Skipping item {} exists...'.format(fname))
+                print(' * Skipping item {} exists...'.format(f_path))
             return True
         return False
