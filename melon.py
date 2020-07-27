d0fd64dc59a8accf5cc5aec7121f4f2ea9edb8f1
Chris McDowall <chris.mcdowall@gmail.com>
2020-07-28 10:02:28 +1200

    Tidy and added one-line csv

--- melon.py
+++ melon.py
@@ -1,42 +1,51 @@
-# -*- coding: utf-8 -*-
+import csv
+import json
+import os
+import time
+from io import StringIO
 
-import csv, json, os, time
 try:
     import requests
 except ImportError:
-    print('* Missing the requests library. Some functions not available.')
+    print("* Missing the requests library. Some functions not available.")
+
 
 def set_default(obj):
     if isinstance(obj, set):
         return list(obj)
     raise TypeError
 
-def touch(f_path='index.html', times=None):
-    with open(f_path, 'a'):
+
+def touch(f_path="index.html", times=None):
+    with open(f_path, "a"):
         os.utime(f_path, times)
 
-def get_filenames(f_dir, prefix='', suffix=''):
+
+def get_filenames(f_dir, prefix="", suffix=""):
     """Get list of filenames within a directory. Optionally scope by prefix/suffix."""
     f_names = []
-    for r,d,files in os.walk(f_dir):
+    for r, d, files in os.walk(f_dir):
         for f in files:
             if f.startswith(prefix) and f.endswith(suffix):
-                f_names.append('{}/{}'.format(r, f))
+                f_names.append("{}/{}".format(r, f))
     return f_names
+
 
 def file_exists(f_path):
     if os.path.isfile(f_path):
         return True
     return False
 
+
 def get_json(url, payload=None, verbose=True):
     """ Get JSON from an online service with optimal URL parameters. """
     r = requests.get(url, params=payload)
     if verbose:
-        print('Fetched: {}'.format(r.url))
+        print("Fetched: {}".format(r.url))
     if r.status_code != 200:
-        print('Error: {}.'.format(r.status_code))
+        print("Error: {}.".format(r.status_code))
     return r.json()
+
 
 def read_textfile(f_path):
     """ Open a textfile and return contents as a stripped list."""
@@ -44,49 +53,62 @@ def read_textfile(f_path):
         rows = f.readlines()
     return [row.strip() for row in rows]
 
-def read_csv(f_path, fieldnames=[], strip_header=False, skip_rows=0):
+
+def read_csv(f_path, fieldnames=[], strip_header=False):
     with open(f_path) as f:
         # optionally strip whitespace from header values (e.g. '  depth' -> 'depth')
         if not fieldnames and strip_header:
-            fieldnames = [h.strip() for h in f.readline().split(',')]
+            fieldnames = [h.strip() for h in f.readline().split(",")]
         if not fieldnames:
             reader = csv.DictReader(f)
         else:
             reader = csv.DictReader(f, fieldnames=fieldnames)
-        # Optionall skip n rows. Useful in combination with fieldnames
-        for n in range(skip_rows):
-            next(reader)
-        rows = [ row for row in reader ]
+        rows = [row for row in reader]
     return rows
 
+
+def parse_one_line_csv(s):
+    """ Parses one-line CSV-like strings to list
+    e.g: 'value1,"oh look, an embedded comma",value3'
+    """
+    file_like_obj = StringIO(s)
+    reader = csv.reader(file_like_obj)
+    rows = [row for row in reader]
+    return rows[0]
+
+
 def read_json(f_path):
-    with open(f_path, 'r') as infile:
+    with open(f_path, "r") as infile:
         data = json.load(infile)
-    print('Read {}.'.format(f_path))
+    print("Read {}.".format(f_path))
     return data
 
+
 def write_json(data, f_path):
-    with open(f_path, 'w') as outfile:
+    with open(f_path, "w") as outfile:
         json.dump(data, outfile, indent=2, sort_keys=True)
-    print('{} written.'.format(f_path))
+    print("{} written.".format(f_path))
+
 
 def write_text(data, f_path):
-    with open(f_path, 'w') as f:
+    with open(f_path, "w") as f:
         try:
-            f.write(data.encode('utf8'))
+            f.write(data.encode("utf8"))
         except TypeError:
             f.write(data)
-    print('Wrote {}.'.format(f_path))
+    print("Wrote {}.".format(f_path))
+
 
 def listdict_to_csv(data, f_path):
-    with open(f_path, 'w') as output_file:
+    with open(f_path, "w") as output_file:
         dict_writer = csv.DictWriter(output_file, data[0].keys())
         dict_writer.writeheader()
         dict_writer.writerows(data)
-    print('Wrote {} with {} rows.'.format(f_path, len(data)))
-    
+    print("Wrote {} with {} rows.".format(f_path, len(data)))
+
+
 def list_to_csv(data, f_path):
-    with open(f_path, 'w') as f:
+    with open(f_path, "w") as f:
         writer = csv.writer(f)
         writer.writerows(data)
-    print('Wrote {} with {} rows.'.format(f_path, len(data) - 1))
+    print("Wrote {} with {} rows.".format(f_path, len(data) - 1))
--- test.py
+++ test.py
@@ -1,57 +1,79 @@
+import os
+import sys
 from random import randint
 import melon as farmer
 from pprint import pprint as pp
 
-print('\n=== STARTING TESTS ===')
+
+print("\n=== STARTING TESTS ===")
 # test getting json
-url = 'https://api.github.com/users/fogonwater/repos'
+url = "https://api.github.com/users/fogonwater/repos"
 d1 = farmer.get_json(url)
-print('Retrieved Github repo info with {} entries.'.format(len(d1)))
-print('---')
+print("Retrieved Github repo info with {} entries.".format(len(d1)))
+print("---")
+
+if os.path.isdir("data"):
+    print("Tests written to 'data' directory")
+else:
+    print(
+        "* WARNING: Remaining tests require a directory called 'data'. Exiting early.."
+    )
+    print("Try: mkdir data")
+    sys.exit()
 
 # write json from http request to file
-farmer.write_json(d1, 'data/test1.json')
-print('---')
+farmer.write_json(d1, "data/test1.json")
+print("---")
 
 
 # write dictionary to file & read it back
-farmer.write_json({'foo' : 'bar', 'fruit_set':['apple', 'apple', 'orange']}, 'data/test2.json')
-print('---')
-d2 = farmer.read_json('data/test2.json')
+farmer.write_json(
+    {"foo": "bar", "fruit_set": ["apple", "apple", "orange"]}, "data/test2.json"
+)
+print("---")
+d2 = farmer.read_json("data/test2.json")
 pp(d2)
-print('---')
+print("---")
 
-#get filenames for a directory
-json_files = farmer.get_filenames('data', suffix='.json')
+# get filenames for a directory
+json_files = farmer.get_filenames("data", suffix=".json")
 pp(json_files)
-print('---')
+print("---")
 
 # test if files exist
-json_files.append('red_herring.json')
+json_files.append("red_herring.json")
 for f_name in json_files:
     if farmer.file_exists(f_name):
         print("{} exists.".format(f_name))
     else:
         print("{} does not exist.".format(f_name))
-print('---')
+print("---")
 
 # post json to url
 # farmer.post_json({'foo' : 'bar'}, 'http://example.com')
 
 # write list of lists to disc and read it back
-report = [['a', 'b', 'c']]
+report = [["a", "b", "c"]]
 for num in range(6):
-    report.append([randint(0,9), randint(0,9), randint(0,9)])
-farmer.list_to_csv(report,'data/test.csv')
-csv_data = farmer.read_csv('data/test.csv')
+    report.append([randint(0, 9), randint(0, 9), randint(0, 9)])
+farmer.list_to_csv(report, "data/test.csv")
+csv_data = farmer.read_csv("data/test.csv")
 pp(csv_data)
-print('---')
+print("---")
 
-farmer.write_text('Hello, World\n  something else here?', 'data/written_text.txt')
-print('---')
+farmer.write_text("Hello, World\n  something else here?", "data/written_text.txt")
+print("---")
 
+
+farmer.write_text("1,2,3\n4,5,6", "data/test_nohead.csv")
 # read a headerless csv
-csv_data = farmer.read_csv('data/test_nohead.csv', fieldnames=['foo','bar','blah'])
+csv_data = farmer.read_csv("data/test_nohead.csv", fieldnames=["foo", "bar", "blah"])
+print('Reading a CSV with no fieldnames')
 pp(csv_data)
-print('---')
-print('=== END TESTS ===')
+print("---")
+
+s = 'value1,"oh look, an embedded comma",value3'
+print('Parsing a one-line string to CSV: {}'.format(s))
+pp(farmer.parse_one_line_csv(s))
+
+print("=== END TESTS ===")
